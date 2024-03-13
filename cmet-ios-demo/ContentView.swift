5ddcf49fd34ff76f3a0159c9f30607d2a2e773ee
joaodcp <67809513+joaodcp@users.noreply.github.com>
2024-03-13 08:25:09 +0000

    Initial Commit

---
+++ cmet-ios-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cmet-ios-demo
+//
+//  Created by João Pereira on 13/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cmet-ios-demo/cmet_ios_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cmet_ios_demoApp.swift
+//  cmet-ios-demo
+//
+//  Created by João Pereira on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct cmet_ios_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
