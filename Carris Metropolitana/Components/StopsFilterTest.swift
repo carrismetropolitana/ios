24843d6fc1e1664741662b2e596b817949c2a8d9
joaodcp <67809513+joaodcp@users.noreply.github.com>
2024-08-19 02:36:02 +0100

    Name changes on file metadata, fly to user coordinates button funtcionality and fav stop widget ETAs appear instantly (bugfix)

---
+++ Carris Metropolitana/Components/StopsFilterTest.swift
@@ -0,0 +1,18 @@
+//
+//  StopsFilterTest.swift
+//  Carris Metropolitana
+//
+//  Created by João Pereira on 19/08/2024.
+//
+
+import SwiftUI
+
+struct StopsFilterTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StopsFilterTest()
+}
