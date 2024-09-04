a52c9dfbd7c72f42d260ef5aa76938c47d62bb35
joaodcp <67809513+joaodcp@users.noreply.github.com>
2024-09-04 23:40:03 +0100

    feat: other agency lines recognizer for external search suggestion

--- Carris Metropolitana/Utils/OtherAgencies.swift
+++ Carris Metropolitana/Utils/OtherAgencies.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+
+func isCcflLine(_ line: String) -> Bool {
+    // Ensure the line is exactly 3 characters long and uppercase it
+    guard line.count == 3 else { return false }
+    let uppercasedLine = line.uppercased()
+    
+    let characters = Array(uppercasedLine)
+    let firstChar = characters[0]
+    let secondChar = characters[1]
+    let thirdChar = characters[2]
+    
+    // Check if the first character is a non-zero digit
+    guard firstChar.isNumber, firstChar != "0" else { return false }
+    
+    if firstChar == "7" || firstChar == "2" {
+        // If all characters are digits, check that the last two aren't "00"
+        if characters.allSatisfy({ $0.isNumber }) {
+            return !(secondChar == "0" && thirdChar == "0")
+        }
+    }
+    
+    // If the third character is a letter, it must be E or B, and the first two must be digits
+    if thirdChar.isLetter && (thirdChar == "E" || thirdChar == "B") {
+        return secondChar.isNumber
+    }
+    
+    return false
+}
